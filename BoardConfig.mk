f885a942c1d5bd3959b1f418cdbe6114384bd4af
androidlover5842 <androidlover5842@gmail.com>
2018-04-12 12:16:04 +0530

    add f2fs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,6 +34,7 @@ TW_EXCLUDE_SUPERSU := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_HAS_DOWNLOAD_MODE := true
 TW_NO_REBOOT_BOOTLOADER := true
+TARGET_USERIMAGES_USE_F2FS := true
 BOARD_CUSTOM_BOOTIMG_MK := device/generic/twrpbuilder/seEnforcing.mk
 include $(LOCAL_PATH)/kernel.mk
 include device/generic/twrpbuilder/BoardConfig32.mk
